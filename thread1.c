aa8de0cf750f57773bf092f2946433fefde4accf
Aditya Pundkar <aditypundkar23@gmail.com>
2025-08-08 22:40:00 +0530

    Update thread1.c

--- thread1.c
+++ thread1.c
@@ -5,13 +5,18 @@
 
 
 void* fun(){
-    printf("Hi thread");
+    printf("Hi thread.\n");
+    sleep(3);
+    printf("Ending thread.\n");
 }
 
 int main(int argc, char const *argv[])
 {
     pthread_t t1;
+    pthread_t t2;
     pthread_create(&t1,NULL,&fun,NULL);
+    pthread_create(&t2,NULL,&fun,NULL);
     pthread_join(t1,NULL);
+    pthread_join(t2,NULL);
     return 0;
 }
